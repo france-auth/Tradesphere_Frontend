540331dfb4c3dd91547ca1a6f2770d2fb8013bbd
Francis <francis.i.authority@gmail.com>
2025-02-17 12:29:05 +0100

    my changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './index.css'
+import AmazingFeatures from './LandingPage/sections/AmazingFeatures'
+import BusinessSection from './LandingPage/sections/BusinessSection'
+import FAQ from './LandingPage/sections/FAQ'
+import FeaturedBrands from './LandingPage/sections/FeaturedBrands'
+import Footer from './LandingPage/sections/Footer'
+import Hero from './LandingPage/sections/Hero'
+import InfiniteScrollLogos from './LandingPage/sections/InfiniteScrollLogos'
+import InfiniteScrollText from './LandingPage/sections/InfiniteScrollText'
+import FeatureSection from './LandingPage/sections/Mission'
+import ProductMarketSection from './LandingPage/sections/ProductMarketSection'
+import Waitlist from './LandingPage/sections/Waitlist'
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Hero />
+      <InfiniteScrollLogos />
+      <FeatureSection />
+      <FeaturedBrands />
+      <AmazingFeatures />
+      <BusinessSection />
+      <InfiniteScrollText />
+      <ProductMarketSection />
+      <InfiniteScrollText />
+      <FAQ />
+      <Waitlist />
+      <Footer />
+    </div>
   )
 }
 
